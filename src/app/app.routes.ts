a02182ab1a2e249e41188ecd456b873e122de188
kautharalhashmi <kathiialhashmi@gmail.com>
2025-06-30 08:15:06 +0400

    angular project initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Signin } from './pages/signin/signin';
+import { Signup } from './pages/signup/signup';
+export const routes: Routes = [
+  { path: '', component: Home },
+   { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+    { path: 'signin', component: Signin },
+  { path: 'signup', component: Signup },
+];
 
-export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
